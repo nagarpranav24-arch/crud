e465a08055509e4f70939d5eaf75d3b73c9aa322
pushkar <nagarpranav24@gmail.com>
2026-05-14 20:13:24 +0200

    crudupdated

--- base/models.py
+++ base/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    message = models.TextField(blank=True)
--- base/views.py
+++ base/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from base.models import Person
 
 # Create your views here.
 
 
 def index(request):
+    name = None
+    email = None
+    message = None
+    if request.method == "POST":
+        name =request.POST.get("name")
+        email =request.POST.get("email")
+        message =request.POST.get("message")
+    
+    person = Person.objects.create(name= name, email=email, message=message)
+    person.save()
+
     return render(request, "index.html")
