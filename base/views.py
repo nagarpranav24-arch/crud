3a8f9e15c614ed878939a19428956e72c5d72709
pushkar <nagarpranav24@gmail.com>
2026-05-19 20:58:56 +0200

    update

--- base/views.py
+++ base/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import redirect, render
 from base.models import Person
 
 # Create your views here.
-
-
 def index(request):
     name= None
     email = None
@@ -25,3 +23,13 @@ def delete_data(request, person_id):
     person = Person.objects.get(id=person_id)
     person.delete()
     return redirect('person_details')
+
+def update_data(request, person_id):
+    person = Person.objects.get(id=person_id)
+    if request.method == "POST":
+        person.name =request.POST.get("name")
+        person.email =request.POST.get("email")
+        person.message =request.POST.get("message")
+        person.save()
+        return redirect("person_details")
+    return render(request, "update.html", {"person":person})
--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name="homepage"),
     path('person-detail/', get_data, name="person_details"),
-    path('delete-person/<int:person_id>', delete_data, name="delete_person")
+    path('delete-person/<int:person_id>', delete_data, name="delete_person"),
+    path('update-person/<int:person_id>', update_data, name="update_person")
 ]
